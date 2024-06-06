492208aaa73f02f1352bc00448ea5d54a04fdf4d
evanahyun <evan_ahyun@icloud.com>
2024-06-06 09:57:38 -0700

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Evan Ah-Yun on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
